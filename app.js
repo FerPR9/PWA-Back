10624bc9733b7fc9165403d9bd4d8c1ec1fa0d4e
FerPR9 <ferp14406@gmail.com>
2024-12-01 22:25:53 -0600

    Suviendo cambios de back

---
+++ app.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var logger = require('morgan');
+var areaRouter = require('./routes/area');
+var indexRouter = require('./routes/index');
+var registroRouter = require('./routes/registro');
+var tallerRouter = require('./routes/taller');
+var conferenciaRouter = require('./routes/conferencia');
+
+var app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const mongo = require('./config/dbconfig');
+
+const cors = require('cors');
+app.use(cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use('/', indexRouter);
+app.use('/registro', registroRouter);
+app.use('/area', areaRouter);
+app.use('/taller', tallerRouter);
+app.use('/conferencia', conferenciaRouter);
+
+module.exports = app;
+
